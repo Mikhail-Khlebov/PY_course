0f4cbe6bc0f0aecc742324f12d45ce4c2b8c8f2e
Mikhail_Kl <277572@mail.ru>
2023-10-10 13:24:43 +0300

    сохранение2

--- 1.py
+++ 1.py
@@ -1,7 +1,65 @@
-print (1)
+# print('нам нужно ввести первое число: ')
+# a = int(input())
+# b = int(input('Введите второе число: '))
+# print(a, '+', b, '=', a + b)
 
-print (2)
-print (3)
-print (4)
-print (5)
-print (6)
+# c = 1.89
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+
+# a = 5.89956
+# b = 6.556551
+# print(round(a*b, 3))
+# round(1.56566, 2)
+
+# iter = 2
+# iter += 3 # iter = iter + 3
+# iter -= 4 # iter = iter - 4
+# iter *= 5 # iter = iter * 5
+# iter /= 5 # iter = iter / 5
+# iter //= 5 # iter = iter // 5
+# iter %= 5 # iter = iter % 5
+# iter**= 5 # iter = iter ** 5
+
+# a = 1 > 4
+# print(a)
+# a = 1 < 4 and 5 > 2
+# print(a)
+# a = 1 == 2
+# print(a)
+# a = 1 != 2
+# print(a)
+# a = 'qwe'
+# b = 'qwe'
+# print(a == b)
+# a = 1 < 3 < 5 < 10
+# print (a)
+
+# username = input ('Напиши имя:')
+# if username == 'Маша':
+#     print('Ура, это МАША!')
+# elif username == 'Марина':
+#     print('Марина цветом как малина')
+# elif username == 'Гузелл':
+#     print('Гузелл возьми себе чупочупс иксиксэл')
+# else:
+#     print('Привет, ', username)
+
+# a = 'qwerty'
+# for i in a:
+#     print(i)
+# # print (a[2])
+ 
+# line = ""
+# for i in range(5):
+#     line = ""
+#     for j in range(5):
+#         line += "+"
+#         print(line)
+
+text = 'Съешь еще картофиля и сходи в туалеТ'
+# print(len(text)) #размер страки
+print('еще'(text)) #есть ли такое значение true\false
