5338a9e4b083267567faba65a8fd0f67d57fcb40
Mikhail_Kl <277572@mail.ru>
2023-12-11 21:05:43 +0500

    сохранение4

--- 1.py
+++ 1.py
@@ -172,3 +172,257 @@ print(n)
 #     print("YES")
 # else:
 #     print("NO")
+# ДЗ
+# n = 123 -> res = 6 (1 + 2 + 3)
+
+# n = 100 -> res = 1 (1 + 0 + 0)
+
+# 
+
+# Задача №11. Решение в группах
+# Дано натуральное число A > 1. Определите, каким по
+# счету числом Фибоначчи оно является, то есть
+# выведите такое число n, что φ(n)=A. Если А не
+# является числом Фибоначчи, выведите число -1.
+# Input: 5
+# Output: 6 
+
+# n = int(input('Введите число больше 1: '))
+
+# count = 1
+# fib1 = 0
+# fib2 = 1
+# temp = 0
+
+# while fib2 <= n:
+#     temp = fib1
+#     fib1 = fib2
+#     fib2 += temp
+#     count += 1
+# if n == fib1:
+#     print(count)
+# else:
+#     print(-1)
+    
+# n = int(input("Введите число больше 1:"))
+
+# count = 4
+# fib1 = 1
+# fib2 = 1
+# fib_res = 2
+
+# while fib_res < n:    
+#     fib1 = fib2
+#     fib2 = fib_res
+#     fib_res = fib2 + fib1
+#     count += 1
+
+# if fib_res == n:
+#     print(count)
+# else:
+#     print(-1)
+
+# Пользователь вводит число N (1 ≤ N ≤ 10). 
+# Далее построчно N чисел от -50 до 50. 
+# Нужно вывести наибольшее количество идущих подряд положительных 
+# чисел.
+# 1
+# Input:    6 -> -20 30 -40 50 10 -10
+# Output: 2
+# import random
+
+# num = int (input("Введите кол-во дней: "))
+# count = 0
+# max_count = 0
+# for i in range(num):
+#     temp = random.randint(-50, 50)
+#     print(temp, end= " ")
+#     if temp <= 0:
+#         count += 1
+#         if max_count < count:
+#             max_count = count
+#     else:
+#         count = 0
+# print()
+# print(max_count)
+# 2
+# import random
+
+# num = int(input("Введите кол-во дней от 1 до 10: "))
+# count = 0
+# max_count = 0
+
+# for i in range(num):
+#     temperature = random.randint(-50, 50)
+#     print(temperature, end= " ") 
+#     if temperature > 0:
+#       count += 1      
+#     else:
+#        if max_count < count:
+#         max_count = count
+#        count = 0
+
+# if max_count < count:
+#     max_count = count       
+# print()       
+# print(max_count)
+
+# Задача №15. Решение в группах
+# 15. Иван Васильевич пришел на рынок и решил
+# купить два арбуза: один для себя, а другой для тещи.
+# Понятно, что для себя нужно выбрать арбуз
+# потяжелей, а для тещи полегче. Но вот незадача:
+# арбузов слишком много и он не знает как же выбрать
+# самый легкий и самый тяжелый арбуз? Помогите ему!
+
+# Пользователь вводит одно число N – количество
+# арбузов. Вторая строка содержит N чисел,
+# записанных на новой строчке каждое. Здесь каждое
+# число – это масса соответствующего арбуза
+# Input: 5 -> 5 1 6 5 9
+# Output: 1 9
+
+# from random import randint
+
+# count = input("Введение кол-ва чисел:")
+
+# max_num = 0
+# min_num = 1000
+
+# for _ in range(count):
+#     current_num = randint(1, 10)
+#     print(current_num, end=" ")
+#     if max_num < current_num:
+#         max_num = current_num
+#     if min_num > current_num:
+#         min_num = current_num
+# print()
+# print(min_num, max_num)
+# 1
+# from random import randint
+
+# count = int(input("Введите количество чисел"))
+
+# max_num = 0
+# min_num = 1000
+
+# for _ in range(count):
+#     current_num = randint(1, 10)
+#     print(current_num, end=" ")
+#     if max_num < current_num: 
+#         max_num = current_num
+#     if min_num > current_num:
+#         min_num = current_num
+        
+
+# print()
+# print
+# 2
+# from random import randint
+
+
+# count = int(input("Введите количество чисел"))
+# current_num = randint(1, 10)
+# max_num = current_num
+# min_num = current_num
+
+# for _ in range(count - 1):
+#     current_num = randint(1, 10)
+#     print(current_num, end=" ")
+#     max_num = max(max_num, current_num)
+#     min_num = min(min_num, current_num)
+        
+
+# print()
+# print(min_num, max_num)
+
+# Задача №17. Решение в группах
+# Дан список чисел. Определите, сколько в нем
+# встречается различных чисел.
+# Input: [1, 1, 2, 0, -1, 3, 4, 4]
+# Output: 6
+
+# from random import randint
+
+# size = int (input('Ввидите размер списка: '))
+# # list_1 = []
+# # for _ in range(size):
+# #     list_1.append(randint(-5,5))
+    
+# list_2 = [randint(-5,5) for _ in range(size)]
+# print(list_2)
+# # uniqe_nums = set(list_2)
+# # print(len(uniqe_nums))
+# print(len(set(list_2)))
+# print(len(set([randint(-5,5) for _ in range(size)])))
+
+# Задача №19. Решение в группах
+# Дана последовательность из N целых чисел и число
+# K. Необходимо сдвинуть всю последовательность
+# (сдвиг - циклический) на K элементов вправо, K –
+# положительное число.
+# Input: [1, 2, 3, 4, 5] k = 3
+# Output: [4, 5, 1, 2, 3]
+
+# from random import randint
+
+# list_1 = [i for i in range(1,randint(7,10))]
+# print (list_1)
+
+# k = int(input('введите число: '))
+# # print(list_1.pop())
+# # for _ in range(k):
+# #     last_el = list_1.pop()
+# #     list_1.insert(0, last_el)
+# # print(list_1)
+# print(list_1[-k:] + list_1[:-k])
+
+# Задача №21. Решение в группах
+# Напишите программу для печати всех уникальных
+# значений в словаре.
+# Input: [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"},
+# {"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII
+# ":" S007 "}]
+# Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
+# 1
+# list_dicts = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "},
+#               {" VIII ":" S007 "}]
+
+# unique_values = set()
+
+# for cur_dict in list_dicts:
+#     for key in cur_dict:
+#         unique_values.add(cur_dict[key].strip())
+        
+# print(unique_values)
+
+# # 2
+# list_dicts = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "},
+#               {" VIII ":" S007 "}]
+
+# unique_values = set()
+
+# for cur_dict in list_dicts:
+#     for key in cur_dict.keys():
+#         unique_values.add(cur_dict[key].strip())
+        
+# print(unique_values)
+
+# Задача №23. Общее обсуждение
+# Дан массив, состоящий из целых чисел. Напишите
+# программу, которая подсчитает количество
+# элементов массива, больших предыдущего (элемента
+# с предыдущим номером)
+# Input: [0, -1, 5, 2, 3]
+# Output: 2 (-1 < 5, 2 < 3)
+# Примечание: Пользователь может вводить значения
+# списка или список задан изначально.
+
+# array = [0, -1, 5, 2, 3]
+# count = 0
+
+# for i in range(len(array) -1):
+#     if array[i] < array[i + 1]:
+#         count += 1
+# print(count)
+
